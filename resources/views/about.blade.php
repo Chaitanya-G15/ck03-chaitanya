79e6b45fdfe9f5532a9b0bebd51f411e55099145
Chaitanya Kshirsagar <kshirsagarcm@rknec.edu>
2024-09-21 22:49:32 +0530

    ck03-chaitanya:About Page Updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,11 +2,35 @@
 <section class="bg-white py-16">
         <div class="container mx-auto text-center">
             <h2 class="text-4xl font-bold text-dark mb-8">About Me</h2>
-            <p class="text-lg text-gray-600 mb-6">
-                I'm a passionate software developer with expertise in web development, backend systems, and
-                cloud technologies. I love solving complex problems and building scalable solutions.
+            <p class="text-2xl text-gray-700 mb-6 font-bold text-pretty">
+            I’m a passionate software developer with a strong interest in web development and coding, dedicated to building efficient, scalable, and user-friendly web applications.
             </p>
-            <img src="https://via.placeholder.com/300" alt="Profile Picture" class="mx-auto rounded-full shadow-lg">
+            <img src="https://img.freepik.com/premium-vector/professional-user-icon-from-primitive-set-isolated-flat-design_1322553-60777.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1726876800&semt=ais_hybrid" alt="Profile Picture" class="mx-auto rounded-full shadow-lg">
+        </div>
+    </section>
+
+    <section class="bg-gray-100 py-16">
+        <div class="container mx-auto text-center">
+            <h2 class="text-4xl font-bold text-dark mb-8">My Skills</h2>
+            <div class="flex justify-center space-x-8">
+                <div class="bg-white p-6 shadow-lg rounded-lg transition-transform transform hover:scale-105 hover:shadow-2xl hover:bg-primary">
+                    <h3 class="text-xl font-semibold mb-4">Frontend Development</h3>
+                    <p class="text-gray-600">HTML, CSS, JavaScript,Tailwind CSS</p>
+                </div>
+                <div class="bg-white p-6 shadow-lg rounded-lg transition-transform transform hover:scale-105 hover:shadow-2xl hover:bg-primary">
+                    <h3 class="text-xl font-semibold mb-4">Backend Development</h3>
+                    <p class="text-gray-600">Laravel, Node.js, PHP</p>
+                </div>
+                <div class="bg-white p-6 shadow-lg rounded-lg transition-transform transform hover:scale-105 hover:shadow-2xl hover:bg-primary">
+                    <h3 class="text-xl font-semibold mb-4">Cloud</h3>
+                    <p class="text-gray-600">Git/Github</p>
+                </div>
+                <div class="bg-white p-6 shadow-lg rounded-lg transition-transform transform hover:scale-105 hover:shadow-2xl hover:bg-primary">
+                    <h3 class="text-xl font-semibold mb-4">Programming Languages</h3>
+                    <p class="text-gray-600">C/C++, Java, Python</p>
+                </div>
+
+            </div>
         </div>
     </section>
     </x-layout> 
